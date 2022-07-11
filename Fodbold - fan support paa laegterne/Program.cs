98db8d219cc775a500578cf97402c101d3ead7ae
Nicklas <nicklashelsberg@gmail.com>
2022-07-11 10:26:34 +0200

    Tilføjet kommentarer

--- Fodbold - fan support paa laegterne/Program.cs
+++ Fodbold - fan support paa laegterne/Program.cs
@@ -12,22 +12,22 @@ namespace Fodbold___fan_support_paa_laegterne
         {
             do
             {
-                int afleveringer;
-                string mål, hf = "High five", ju = "Jubel!!!", hu = "Huh!", sh = "Shh", ol = "Olé olé olé";
+                int afleveringer; //opret int variable
+                string mål, hf = "High five", ju = "Jubel!!!", hu = "Huh!", sh = "Shh", ol = "Olé olé olé"; //opret 6 string variabler
                 do
                 {
-                    try
+                    try //prøv at konvertere input til 32 bit integer
                     {
                         Console.Write("\nAntal aflerveringer: ");
                         afleveringer = Convert.ToInt32(Console.ReadLine());
                         break;
                     }
-                    catch (FormatException)
+                    catch (FormatException) //catch hvis input ikke er tal
                     {
                         Console.WriteLine("input må kun være et tal");
                         continue;
                     }
-                    catch (Exception ex)
+                    catch (Exception ex) //catch generel fejl
                     {
                         Console.WriteLine(ex.Message);
                         continue;
@@ -38,24 +38,26 @@ namespace Fodbold___fan_support_paa_laegterne
 
                 if (mål == "mål")
                 {
-                    Console.WriteLine(ol);
+                    Console.WriteLine("\n{0}",ol); //skriv Olé olé olé hvis brugeren har skrevet mål
                 }
                 else if (afleveringer == 0)
                 {
-                    Console.WriteLine(sh);
+                    Console.WriteLine("\n{0}",sh); //skriv Shh hvis brugeren har angivet 0 afleveringer
                     continue;
                 }
                 else if (afleveringer <= 10)
                 {
-                    for (int i = 0; i < afleveringer; i++)
+                    Console.WriteLine("\n");
+                    for (int i = 0; i < afleveringer; i++) //loop de antal gange brugeren har skrevet i afleveringer
                     {
-                        Console.Write("{0} ", hu);
+                        Console.Write("{0} ", hu); //skriv Huh!
                     }
+                    Console.WriteLine("\n");
                     continue;
                 }
                 else
                 {
-                    Console.WriteLine($"{hf} - {ju}");
+                    Console.WriteLine($"\n{hf} - {ju}"); //skriv High five og Jubel!!! hvis brugeren har angivet over 10 afleveringer
                     continue;
                 }
             } while(true);
